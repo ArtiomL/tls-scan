beda71d96645462e8fbaa0cfacf326e4fc7c7bd0
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-11-07 15:47:34 +0200

    Logging

--- lib/log.py
+++ lib/log.py
@@ -1,1 +1,29 @@
-test
+#!/usr/bin/env python
+# tls-scan - lib: Logging
+# https://github.com/ArtiomL/tls-scan
+# Artiom Lichtenstein
+# v1.0.0, 07/11/2016
+
+import subprocess
+import time
+
+__author__ = 'Artiom Lichtenstein'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+
+# Log level to /var/log/messages (or stdout)
+intLogLevel = 0
+strLogMethod = 'log'
+strLogID = '[-v%s-161107-] - ' % __version__
+
+# Logger command
+strLogger = 'logger -p local0.'
+
+def funLog(intMesLevel, strMessage, strSeverity = 'info'):
+	if intLogLevel >= intMesLevel:
+		if strLogMethod == 'stdout':
+			print('%s %s' % (time.strftime('%b %d %X'), strMessage))
+		else:
+			lstCmd = (strLogger + strSeverity).split(' ')
+			lstCmd.append(strLogID + strMessage)
+			subprocess.call(lstCmd)
