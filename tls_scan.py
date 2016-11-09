3630d172e894592b314dde18e92927861799272e
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-11-09 12:33:07 +0200

    Update tls_scan.py

--- tls_scan.py
+++ tls_scan.py
@@ -2,6 +2,23 @@
 # tls-scan - Automated TLS/SSL Server Tests for Multiple Hosts
 # https://github.com/ArtiomL/tls-scan
 # Artiom Lichtenstein
-# v0.0.0, 08/11/2016
+# v0.0.1, 08/11/2016
 
-from lib import log, reapi
+import argparse
+import atexit
+import datetime
+import json
+import lib.cfg as cfg
+import lib.log as log
+import lib.reapi as reapi
+import os
+import requests
+import signal
+import socket
+import subprocess
+import sys
+import time
+
+__author__ = 'Artiom Lichtenstein'
+__license__ = 'MIT'
+__version__ = '0.0.1'
