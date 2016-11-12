1956cde07c46a7581eec2418d5963c8427329587
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-11-13 00:48:59 +0200

    Update reapi.py

--- lib/reapi.py
+++ lib/reapi.py
@@ -2,7 +2,7 @@
 # tls-scan - lib: REST API
 # https://github.com/ArtiomL/tls-scan
 # Artiom Lichtenstein
-# v0.0.2, 08/11/2016
+# v0.0.3, 12/11/2016
 
 import json
 import log
@@ -10,7 +10,7 @@ import requests
 
 __author__ = 'Artiom Lichtenstein'
 __license__ = 'MIT'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 # SSL Labs REST API
 class clsSAPI(object):
@@ -25,6 +25,8 @@ class clsSAPI(object):
 		self.objHS = requests.session()
 		# Add Content-Type to HTTP headers and modify User-Agent
 		self.objHS.headers.update({ 'Content-Type': 'application/json', 'User-Agent': 'tls-scan v%s' % __version__ })
+		# 
+		self.boolJSON = False
 
 	def funAnalyze(self, strHost):
 		# Initiate a new assessment for a host
@@ -36,24 +38,36 @@ class clsSAPI(object):
 		except Exception as e:
 			log.funLog(2, repr(e), 'err')
 
+	def funGrades(self, diOper):
+		lstGrades = []
+		for diEP in diOper['endpoints']:
+			strStaMess = diEP['statusMessage']
+			strGrade = 'X'
+			if strStaMess == 'Ready':
+				strGrade = diEP['grade']
+			lstGrades.append('%s %s, %s, %s' % (strGrade, diOper['host'], diEP['ipAddress'], strStaMess))
+		return lstGrades
+
 	def funOpStatus(self, strHost):
 		# Check operation status
-		strURL = self.strAPIE + self.strAnalyze + strHost
 		strStatus = 'DNS'
+		strURL = self.strAPIE + self.strAnalyze + strHost
 		while strStatus == 'DNS':
 			try:
 				diOper = json.loads(self.objHS.get(strURL).content)
 				strStatus = diOper['status']
+				log.funLog(3, 'Transaction status: %s' % strStatus)
 				if strStatus == 'READY':
-					return diOper
+					# Cached result (from 'DNS' to 'READY')
+					return diOper if self.boolJSON else self.funGrades(diOper)
 
-				if strStatus == 'ERROR':
+				elif strStatus == 'ERROR':
 					return ['X %s, %s' % (strHost, diOper['statusMessage'])]
 
-				lstMessages = [None] * len(diOper['endpoints'])
-				log.funLog(2, 'Total number of endpoints: %s' % len(lstMessages))
 			except Exception as e:
 				log.funLog(2, repr(e), 'err')
+		lstMessages = [None] * len(diOper['endpoints'])
+		log.funLog(2, 'Total number of endpoints: %s' % len(lstMessages))
 		while strStatus == 'IN_PROGRESS':
 			try:
 				diOper = json.loads(self.objHS.get(strURL).content)
